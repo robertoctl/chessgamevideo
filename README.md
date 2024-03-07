f1056ff348a8e54d33da94b84a76ed07777e96c6
roberto.torres <roberto.torres@wgu.edu>
2024-03-07 13:54:55 -0600

    Fix output video

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# chessgamevideo
+# Chess Game Video
+
+Simple package of a Telegram bot to generate a video (.mp4) from a PGN file 
+
+Link to Telegram bot:
+https://t.me/ChessPgnToVideo_bot
+
+![Sample PGN event](./sampleEvent.pgn)
+
+![Sample output video](./output.mp4)
+
+This package was built using
+
+Fluent ffmpeg [https://github.com/fluent-ffmpeg/node-fluent-ffmpeg]
+
+Chess Image Generator [https://github.com/andyruwruw/chess-image-generator]
--- main.js
+++ main.js
@@ -1,51 +1,193 @@
+const TelegramBot = require('node-telegram-bot-api');
+const AWS = require('aws-sdk');
+const crypto = require("crypto")
+const base64url = require('base64url');
 var ChessImageGenerator = require("chess-image-generator")
 var fs = require('fs')
-const crypto = require("crypto")
-var FFmpeg = require('fluent-ffmpeg');
+var ffmpeg = require('fluent-ffmpeg');
 
-var lineReader = require('readline').createInterface({
-  input: fs.createReadStream('sampleEvent.pgn')
-});
+AWS.config.update({ region: "us-east-1" });
 
-const id = 'work/' + crypto.randomBytes(16).toString("hex");
-fs.mkdirSync(id);
+const s3 = new AWS.S3();
+const ssm = new AWS.SSM()
+const BUCKET_NAME = 'chessgamevideo';
 
-var header = '';
-var index = 0;
+(async() => {
+  const ssm = new AWS.SSM();
+  const parameter = await ssm.getParameter({ 
+      Name: '/chessgamevideo/telegram_bot_token', 
+      WithDecryption: true 
+  }).promise();
 
-lineReader.on('line', async function (line) {
-  const imageGenerator = new ChessImageGenerator({
-    padding: [288, 44, 268, 44],
-    size: 1316
-  });
-  if(!line.startsWith('1')) {
-    header += line + '\n';
-  }
-  else {
-    for(i = 1;;i++) {
-      index = line.indexOf(i+1+'.');
-      if(index != -1) {
-        slice = line.slice(0, index-1);  
-        await imageGenerator.loadPGN(header + slice);
-        await imageGenerator.generatePNG(id + '/pos' + i + '.png');
-      } else {
-        index = line.indexOf('{');
-        slice = line.slice(0, index-1);  
-        await imageGenerator.loadPGN(header + slice);
-        await imageGenerator.generatePNG(id + '/pos' + i + '.png');
-        return;
-      }
+  const token = parameter.Parameter.Value;
+  const bot = new TelegramBot(token, { polling: true });
+  var lastIndex = 0;
+
+  bot.on('message', (msg) => {
+    try {
+      const chatId = msg.chat.id;
+      const messageText = msg.text;
+      main(chatId, messageText);
+    } catch(error) {
+      console.log(error);
     }
-  } 
-});
+  });
 
-lineReader.on('close', function () {  
-  var patternCommand = FFmpeg({ source: id + '/pos%d.png' })
-  .withFPSInput(1)    
-  .withFps(1)
-  .withFpsOutput(1)
-  .on('end', function() {
-    console.log('Processing finished !');
-  })
-  .saveToFile(id + '/output.mp4');
-});
+  async function main(chatId, messageText) {
+    try {
+      console.log("## New event received ##");
+      bot.sendMessage(chatId, 'Generating your video...');
+      path = 'work/' + base64url(crypto.randomBytes(16));
+      await generateImages(path, messageText, chatId)
+      await sleep(1000);
+      await generateVideo(path, chatId);
+      await sendVideo(path, chatId);
+      await saveEventToS3(path, messageText, chatId);
+      console.log("## Success! ##");
+    } catch(error) {
+      console.log(error);
+    } finally {
+      cleanup(path, chatId);
+    }
+  }
+
+  function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  async function generateImages(path, messageText, chatId) {
+    try{
+      console.log('Generate images for path: ' + path + ' and chatId: ' + chatId);
+      fs.mkdirSync(path);
+      var header = '';
+      var index = 0;
+      var arr = messageText.split('\n');
+      const imageGenerator = new ChessImageGenerator({
+        padding: [288, 44, 268, 44],
+        size: 1316
+      });
+      for(i = 0; i < arr.length; i++) {
+        line = arr[i];
+        if(!line.startsWith('1')) {
+          header += line + '\n';
+        }
+        else {
+          for(i = 1;;i++) {
+            index = line.indexOf(i+1+'.');
+            if(index != -1) {
+              slice = line.slice(0, index-1);  
+              await imageGenerator.loadPGN(slice);
+              await imageGenerator.generatePNG(path + '/pos' + i + '.png');
+            } else {
+              index = line.indexOf('{');
+              slice = line.slice(0, index-1);  
+              await imageGenerator.loadPGN(slice);
+              await imageGenerator.generatePNG(path + '/pos' + i + '.png');
+              lastIndex = i;
+              break;
+            }
+          }
+        } 
+      }    
+      console.log('Finished generating images for path: ' + path + ' and chatId: ' + chatId);
+    } catch (error) {
+      console.log(error);
+      bot.sendMessage(chatId, 'Seems not a valid PGN file');
+    }
+  }
+
+  async function generateVideo(path, chatId){
+    try {
+      console.log('Generate video for path: ' + path + ' and chatId: ' + chatId);
+      return new Promise((resolve,reject)=>{
+        var patternCommand = ffmpeg({ source: path + '/pos%d.png' })
+          .inputFPS(1)
+          .fps(5)
+          .videoCodec('libx264')
+          .on('error', function(error) {
+            console.log('Cannot process video: ' + error.message);
+            bot.sendMessage(chatId, 'Error while generating the video 0x01');
+            return reject(error)
+          })
+          .on('end', function() {
+            console.log('Finished generating video for path: ' + path + ' and chatId: ' + chatId);
+          })
+          .save(path + '/output.mp4')
+          .on('end', ()=>{
+            console.log('Finished saving video for path: ' + path + ' and chatId: ' + chatId);
+            return resolve()
+          })
+          .on('err',(error)=>{
+            console.log(error.message);
+            return reject(error)
+          });
+      })
+    } catch(error) {
+      console.log(error.message);
+      bot.sendMessage(chatId, 'Error while generating the video 0x02');
+      return reject(error)
+    }
+  }
+
+  async function sendVideo(path, chatId) {
+    try {
+      console.log('Send video for path: ' + path + ' and chatId: ' + chatId);
+      return new Promise((resolve,reject)=>{
+        bot.sendVideo(chatId, path + '/output.mp4');
+        console.log('Finished sending video for path: ' + path + ' and chatId: ' + chatId);
+        return resolve()
+      });
+    } catch(error) {
+      console.log(error.message);
+      bot.sendMessage(chatId, 'Error while sending the video');
+    }
+  }
+
+  async function saveEventToS3(path, messageText, chatId) {
+    try {
+      console.log('Save event to S3 for path: ' + path + ' and chatId: ' + chatId);
+      const video = fs.readFileSync(path + '/output.mp4');
+      const videoParams = {
+        Bucket: BUCKET_NAME,
+        Key: path + '/output.mp4',
+        Body: video
+      };
+      await s3.upload(videoParams, function(err, data) {if (err) {throw err;}}).promise();
+      
+      const pgnParams = {
+        Bucket: BUCKET_NAME,
+        Key: path + '/event.pgn',
+        Body: messageText
+      };
+      await s3.upload(pgnParams, function(err, data) {if (err) {throw err;}}).promise();
+      
+      const pngLastPosition = fs.readFileSync(path + '/pos' + lastIndex + '.png');
+      const pngParams = {
+        Bucket: BUCKET_NAME,
+        Key: path + '/pos' + lastIndex + '.png',
+        Body: pngLastPosition
+      };
+      await s3.upload(pngParams, function(err, data) {if (err) {throw err;}}).promise();
+
+      console.log('Finished saving event to S3 for path: ' + path + ' and chatId: ' + chatId);
+    
+    } catch(error) {
+      console.log(error.message);
+      bot.sendMessage(chatId, 'General error');
+    }
+  }
+
+  function cleanup(path, chatId) {
+    try {
+      console.log('Cleanup path: ' + path + ' and chatId: ' + chatId);
+      fs.rmSync(path, { recursive: true, force: true });
+      console.log('Finished doing cleanup for path: ' + path + ' and chatId: ' + chatId);
+    } catch(error) {
+      console.log(error.message);
+      bot.sendMessage(chatId, 'Error while performing cleanup');
+    }
+  }
+
+})();
